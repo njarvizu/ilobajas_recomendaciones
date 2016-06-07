c7c1f7713854dfb2a5b09524ee3af5b58b0db33a
Onizuka <ricardohs35@gmail.com>
2016-06-07 12:14:22 -0500

    Login stuff

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,13 @@
 <?php
 
-    Route::get('/{id?}/{action?}', ['uses' => 'DescargasController@historial_descargas']);
-    Route::get('movies/show_movies', 'MoviesController@show_movies');
-    Route::get('movies/rate/{movie_id}/{rating}', 'MoviesController@rate');
+Route::get('/', function() {
+    return view('welcome');
+});
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('movies/download/{id?}/{action?}', ['uses' => 'DescargasController@historial_descargas']);
+
+Route::get('movies/show_movies', 'MoviesController@show_movies');
+Route::get('movies/rate/{movie_id}/{rating}', 'MoviesController@rate');
