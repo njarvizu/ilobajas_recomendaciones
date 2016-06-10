f749d861def7eacb1d0da9c743d0569460d81fb4
Onizuka <ricardohs35@gmail.com>
2016-06-10 14:05:32 -0500

    Guardar user id, relaciones modelo

--- resources/views/movies/show_movies.blade.php
+++ resources/views/movies/show_movies.blade.php
@@ -26,27 +26,36 @@
                           </div>
                           <div class="panel-footer">
                             <a class="btn btn-danger" href="http://youtube.com/watch?v={{ $movie->yt_trailer_code }}" target="_blank"><i class="fa fa-youtube"></i></a>
-                            <div class="btn-group pull-right">
-                                <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="fa fa-star"></i> <span class="caret"></span>
-                                </button>
-                                <ul class="dropdown-menu">
 
-                                    @for ($i = 1; $i < 11; $i++)
-                                        <li><a href="{{ url('movies/rate/'.$movie->id.'/'. $i ) }}">{{ $i }}</a></li>
-                                    @endfor
+                            @if (!count($movie->ratings))
+                                <div class="btn-group pull-right">
+                                    <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fa fa-star"></i> <span class="caret"></span>
+                                    </button>
 
-                                </ul>
-                            </div>
+                                    <ul class="dropdown-menu">
+
+                                        @for ($i = 1; $i < 11; $i++)
+                                            <li><a href="{{ url('movies/rate/'.$movie->id.'/'. $i ) }}">{{ $i }}</a></li>
+                                        @endfor
+
+                                    </ul>
+                                </div>
+                            @else
+                                  <a href="#" class="btn btn-warning pull-right">
+                                      {{ $movie->ratings[0]->rating }} <i class="fa fa-star"></i>
+                                  </a>
+                                {{--<span style="background-color:#f0ad4e;" class="badge pull-right">{{ $movie->ratings[0]->rating }} <i class="fa fa-star"></i></span>--}}
+                            @endif
 
 
 
-                            <div class="Movie--rating">
-
+                            {{--<div class="Movie--rating">--}}
+                                <!-- Check if movie ratings -->
                                 @if (!count($movie->ratings))
-                                    <a href="{{ url('movies/rate/'.$movie->id.'/8') }}"><i style="color:yellow" class="fa fa-star"></i></a>
+{{--                                    <a href="{{ url('movies/rate/'.$movie->id.'/8') }}"><i style="color:yellow" class="fa fa-star"></i></a>--}}
                                 @endif
-                            </div>
+                            {{--</div>--}}
                           </div>
                         </div>
                     </div>
